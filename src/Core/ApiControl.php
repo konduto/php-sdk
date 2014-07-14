fd582fdea83ac99b5603834df5f9efb5fb01e302
Adriano Oliveira <adriano@konduto.com>
2014-07-14 15:23:59 -0300

    Updates annotations, comments, docstrings

--- src/Core/ApiControl.php
+++ src/Core/ApiControl.php
@@ -3,20 +3,36 @@
 use \Konduto\Models as Models;
 use \Konduto\Exceptions as Exceptions;
 
-// Constants used to define API specific configuration (they should change only with versions of the sdk).
+/*
+ * Defines what URL to communicate with.
+ */
 const ENDPOINT        = "https://api.konduto.com/";
+
+/*
+ * Current version of the SDK.
+ */
 const CURRENT_VERSION = "v1";
+
+/*
+ * Number of seconds before giving up of waiting for a response from Konduto API
+ */
 const API_TIMEOUT     = 30;      // In seconds.
 
-// Constants used to switch between HTTP methods
+/*
+ * Constants used for switching between HTTP methods use inside ApiControl class
+ */
 const METHOD_GET  = 0;
 const METHOD_POST = 1;
 const METHOD_PUT  = 2;
 
-// Messages responded by Konduto API
+/*
+ * Important messages that might be responded by Konduto API
+ */
 const MSG_DUPLICATE_ORDER  = "record already exists";
 
-// HTTP status codes
+/*
+ * List of HTTP status codes used by SDK
+ */
 const HTTP_OK             = 200;
 const HTTP_UNAUTHORIZED   = 401;
 const HTTP_FORBIDDEN      = 403;
@@ -24,9 +40,13 @@ const HTTP_NOT_FOUND      = 404;
 const HTTP_INTERNAL_ERROR = 500;
 
 /**
+ * API specific control methods
+ *  
  * This class describes details of the API functioning, such as use of curl library.
  * It provides auxiliary methods to be used by the public methods.
- * All its methods are protected.
+ * All its methods are protected. 
+ *
+ * @ignore
  */ 
 abstract class ApiControl {
 
@@ -35,11 +55,23 @@ abstract class ApiControl {
     protected static $lastResponse;               // String containing last response from a request to Konduto API
 
     /**
-     * This method sends a request of a chosen method, allowing for passing data
-     * as a string, to a relative URL for the ENDPOINT defined.
+     * Method for sending an HTTP request to Konduto API at the specified ENDPOINT
+     *
+     * This method sends an HTTP request to an URL defined by the constant ENDPOINT
+     * adding the API version and a relative path provided in the arguments.
+     * The body of the request is specified by the parameter data.
+     *
      * @param $data a string containing the body of the request.
      * @param $method accepts one of the numerical constants METHOD_* defined above.
      * @param $relative_url the url to be constructed using ENDPOINT and the set version.
+     *
+     * @throws InvalidAPIKeyException if an API key was not set (with setApiKey method) or if Konduto API could not recognize the set key as a valid API key
+     * @throws CommunicationErrorException if there it could not reach the API
+     * @throws TimeoutException if the SDK is waiting for a response for TIMEOUT seconds
+     * @throws KondutoAPIErrorException if the API responds with a message with http status 500 (internal error)
+     * @throws OperationNotAllowedException the operation being performed is not allowed for the set API key
+     *
+     * @return An associative array with the response of the api and the returned status code.
      */
     protected static function sendRequest($data, $method, $relative_url) {
 
@@ -136,17 +168,25 @@ abstract class ApiControl {
 
     /**
      * Checks whether the version provided is valid or not. If it is not valid, throws and exception.
-     * TODO: This function needs to be updated when new versions of the API get available.
+     *
+     * @todo  This function needs to be updated when new versions of the API get available.
+     *
+     * @param version a string containing the version to be set
+     * 
+     * @throws InvalidVersionException if the version cannot be recognized
      */
-    protected static function validate_version($version) { 
+    protected static function validate_version($version) {
         if ($version != CURRENT_VERSION) {
             throw new Exceptions\InvalidVersionException($version);
         }
-        return true;
     }
 
     /**
-     * Returns the status of an order given a recommendation.
+     * Returns the status of an order given a recommendation
+     * 
+     * @param recommendation string
+     *
+     * @return status string
      */
     protected static function get_status($recommendation) {
         switch (strtolower($recommendation)) {
@@ -163,6 +203,11 @@ abstract class ApiControl {
 
     /**
      * Check if the response was successful, throw Exceptions in case of errors.
+     * 
+     * @throws DuplicateOrderException there is a message indicating the order being persisted is duplicate
+     * @throws KonduoAPIErrorException when the API responds nor error neither OK
+     *
+     * @return true if success
      */
     protected static function check_post_response($response, $order_id = null) {
         if ($response["http_status"] == HTTP_OK) {
@@ -179,7 +224,14 @@ abstract class ApiControl {
     }
 
     /**
-     * Check if the order exists.
+     * Check an HTTP response to order found.
+     * 
+     * @param response HTTP
+     * @param order id, to properly build exception if error happens
+     *
+     * @throws OrderNotFoundException if http status is 404
+     * 
+     * @return true if success
      */
     protected static function was_order_found($response, $order_id = null) {
         if ($response["http_status"] == HTTP_OK) {
@@ -195,6 +247,10 @@ abstract class ApiControl {
 
     /**
      * Check whether the provided key is a production key.
+     *
+     * @param key
+     * 
+     * @return true if provided keys is a production key
      */
     protected static function is_production_key($key) {
         return is_string($key) && $key[0] == 'P';
@@ -202,6 +258,8 @@ abstract class ApiControl {
 
     /**
      * Returns the last response from Konduto API.
+     *
+     * @return last response message from Konduto API.
      */
     public static function getLastResponse() {
         return self::$lastResponse;
